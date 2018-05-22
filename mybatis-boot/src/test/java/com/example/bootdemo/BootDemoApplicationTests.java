1a5c038ce58793880601584800c7d929cf071312
陈键钊 <chenjz@77ircloud.com>
2018-05-22 19:52:05 +0800

    init

---
+++ mybatis-boot/src/test/java/com/example/bootdemo/BootDemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.bootdemo;
+
+import com.example.bootdemo.mapper.LearnMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	LearnMapper mapper;
+
+	@Test
+	public void testMapper(){
+
+		System.out.println(mapper.queryLearnResouceById(999l));
+	}
+
+}
---
+++ mybatisplus-boot/src/main/java/com/example/mybatisplusboot/MybatisplusBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.mybatisplusboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusBootApplication.class, args);
+    }
+}
